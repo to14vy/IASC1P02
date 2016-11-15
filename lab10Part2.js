f9f7d17a2fbae73d991c83791c56a618ad61da52
to14vy <to14vy@brocku.ca>
2016-11-14 19:25:02 -0500

    Update lab10Part2.js
    syntax fix

--- lab10Part2.js
+++ lab10Part2.js
@@ -4,4 +4,4 @@ theDate = new Date();
 var year = Number(theDate.getFullYear());
 
 var result = year - myPrompt;
-document.getElementById("year").innerHTML="You were born in the year "+result".";
+document.getElementById("year").innerHTML="You were born in the year "+result;
